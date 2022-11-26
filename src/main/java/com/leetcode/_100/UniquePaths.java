081fb883be179faa77973f5c274510066dc9d563
Vlad Povarna <povarna@adobe.com>
2022-11-26 12:30:17 +0200

    Unique Paths problem

---
+++ src/main/java/com/leetcode/_100/UniquePaths.java
@@ -0,0 +1,25 @@
+package com.leetcode._100;
+
+public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    // initialization
+                    grid[i][j] = 1;
+                } else {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+                }
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        UniquePaths uniquePaths = new UniquePaths();
+        System.out.println(uniquePaths.uniquePaths(3, 7));
+    }
+}
