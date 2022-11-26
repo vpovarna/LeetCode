96920fc1d2ad54dadd80c146314122991130a1b8
Vlad Povarna <povarna@adobe.com>
2022-11-26 12:18:19 +0200

    MinCostClimbingStairs problem Addendum

--- src/main/java/com/leetcode/_700/MinCostClimbingStairs.java
+++ src/main/java/com/leetcode/_700/MinCostClimbingStairs.java
@@ -8,12 +8,26 @@ public class MinCostClimbingStairs {
         int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 0;
-        for (int i = 2; i <= n; i ++) {
-            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2]+ cost[i-2]);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
 
         return dp[n];
     }
+
+    // optimize space complexity
+    public int minCostClimbingStairs2(int[] cost) {
+        int n = cost.length;
+        int f1 = 0, f2 = 0;
+        for (int i = 2; i <= n; i++) {
+            int tmp = Math.min(f1 + cost[i-2], f2 + cost[i-1]);
+            f1 = f2;
+            f2 = tmp;
+        }
+
+        return f2;
+    }
+
 
     public static void main(String[] args) {
         MinCostClimbingStairs minCostClimbingStairs = new MinCostClimbingStairs();
